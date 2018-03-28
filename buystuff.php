43957e42e62f8045ebdab1e48e9c03b10b0fefbb
Tim <tkelly1996@yahoo.com>
2018-03-27 23:35:32 -0400

    php connected to local DB

--- buystuff.php
+++ buystuff.php
@@ -117,3 +117,18 @@
 	</form>
 </body>
 </html>
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "Dogsrock57!";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
